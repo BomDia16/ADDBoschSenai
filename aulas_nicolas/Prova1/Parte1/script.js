16588eddba659a5ac64a056b44a3e5e8ffbc9368
BomDia16 <arturbarbosaoliveira@gmail.com>
2026-03-25 09:12:00 -0300

    Parte1 da prova

---
+++ aulas_nicolas/Prova1/Parte1/script.js
@@ -0,0 +1,60 @@
+let produtos = []
+
+function adicionarProduto() {
+    const nome = document.getElementById("nome").value
+    const preco = document.getElementById("preco").value
+    const quantidade = document.getElementById("quantidade").value
+
+    let produto = {
+        "nome": nome,
+        "preco": preco,
+        "quantidade": quantidade
+    }
+
+    produtos.push(produto)
+
+    console.log(produtos)
+
+    document.getElementById("nome").value = ""
+    document.getElementById("preco").value = ""
+    document.getElementById("quantidade").value = ""
+
+    // maior preco
+
+    let maiorPreco = produtos.reduce((acc, value) => {
+        if (value.preco > acc) {
+            return acc = value.preco
+        } else {
+            return acc
+        }
+    }, 0)
+
+    let maisCaro = produtos.filter(p => p.preco == maiorPreco)
+
+    let pMaiorPreco = document.getElementById("maisCaro")
+    pMaiorPreco.innerHTML = maisCaro[0].nome
+
+    // maior quantidade
+
+    let maiorQuantidade = produtos.reduce((acc, value) => {
+        if (value.quantidade > acc) {
+            return acc = value.quantidade
+        } else {
+            return acc
+        }
+    }, 0)
+
+    let produtoMaiorQuantidade = produtos.filter(p => p.quantidade == maiorQuantidade)
+
+    let pMaiorQuantidade = document.getElementById("maiorQuantidade")
+    pMaiorQuantidade.innerHTML = produtoMaiorQuantidade[0].nome
+
+    // valor total
+
+    let valorTotal = produtos.reduce((acc, value) => {
+        return parseInt(acc) + parseInt(value.preco)
+    }, 0)
+
+    let pValorTotal = document.getElementById("valorTotal")
+    pValorTotal.innerHTML = "R$ " + valorTotal
+}

c39d36afaf42141e98c5010d1e2f4acc80908f47
BomDia16 <arturbarbosaoliveira@gmail.com>
2026-04-22 10:03:52 -0300

    CRUD com javascript e backend

--- aulas_nicolas/aulas-javascript/Aula 15/backend/server.js
+++ aulas_nicolas/aulas-javascript/Aula 15/backend/server.js
@@ -16,18 +16,75 @@ const connection = mysql.createConnection({
     user: "root",
     password: "root",
     host: "localhost",
-    database: "aula_add",
+    database: "aula_add2",
     port: 3307
 })
 
-if (connection) {
-    console.log("Banco de dados conectado")
+if(connection){
+    console.log("Banco de dados conectado!")
 }
 
 app.get('/', (req, res) => {
-    return res.send('Servidor funcionando')
+    return res.send("Servidor funcionando corretamente!")
+})
+
+app.get('/usuarios', (req, res) => {
+    connection.query("SELECT * FROM usuario", (err, results) => {
+        if(err){
+            return
+        }
+        console.log(results)
+        res.status(200).send(results)
+    })
+})
+
+app.get('/usuarios/:id', (req, res) => {
+    const { id } = req.params
+    connection.query("SELECT * FROM usuario WHERE id = ?", 
+        [id], 
+        (err, results) => {
+            if(err){
+                return
+            }
+            return res.status(200).send(results[0])
+    })
+})
+
+app.post('/registro', (req, res) => {
+    const { nome, email } = req.body
+    connection.query("INSERT INTO usuario (nome,email) VALUES (?,?)",
+        [nome, email]
+    )
+
+    return res.status(201).send({ response: "Usuário registrado com sucesso!"})
+})
+
+app.delete('/deletar/:id', (req, res) => {
+    const { id } = req.params
+    
+    try {
+        connection.query('delete from usuario where id = ?', [id])
+        return res.status(200).send({message: 'Usuário deletado com sucesso!'})
+    }
+    catch (e) {
+        return res.status(500).send({error: e})
+    }
+})
+
+app.put('/atualizar/:id', (req, res) => {
+    const { id } = req.params
+    const { nome, email } = req.body
+    try {
+        connection.query('update usuario set nome = ?, email = ? where id = ?',
+            [nome, email, id]
+        )
+        return res.status(200).send({ message: "Usuário atualizado com sucesso" })
+    }
+    catch (e) {
+        return res.status(500).send({ error: e })
+    }
 })
 
 app.listen(port, () => {
-    console.log("Servidor rodando em http://localhost:8090")
+    console.log("Servidor rodando em http://localhost:8080")
 })

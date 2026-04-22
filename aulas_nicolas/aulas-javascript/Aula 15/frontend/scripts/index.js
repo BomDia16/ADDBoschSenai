a3002e45c9a7855f345ac663b903ce9dda0de85c
BomDia16 <arturbarbosaoliveira@gmail.com>
2026-04-22 11:24:23 -0300

    Adicionando front end para crud

---
+++ aulas_nicolas/aulas-javascript/Aula 15/frontend/scripts/index.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+    
+    const response = await fetch('http://localhost:8090/registro', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    
+    const data = await response.json()
+    console.log(data)
+    carregarUsuarios()
+})
+
+async function carregarUsuarios() {
+    const response = await fetch('http://localhost:8090/usuarios')
+    
+    const usuarios = await response.json()
+    
+    const tbody = document.getElementById('listaUsuarios')
+
+    tbody.innerHTML = ""
+    
+    usuarios.forEach(user => {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <td>${user.nome}</td>
+        <td>${user.email}</td>
+        `
+        
+        tbody.appendChild(tr)
+    });
+}
+
+window.onload(carregarUsuarios())

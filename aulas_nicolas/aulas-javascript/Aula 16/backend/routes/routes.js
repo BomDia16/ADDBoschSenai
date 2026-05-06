58b1a9853b66ca6c2156aacc60a3ce000c3a8b4b
BomDia16 <arturbarbosaoliveira@gmail.com>
2026-05-06 11:39:41 -0300

    Exercicio MVC

--- aulas_nicolas/aulas-javascript/Aula 16/backend/routes/routes.js
+++ aulas_nicolas/aulas-javascript/Aula 16/backend/routes/routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import user from './user.js'
 import vehicle from './vehicle.js'
+import pedido from './pedido.js'
 
 export default function(app) {
     app
     .use(express.json())
     .use('/user', user)
     .use('/vehicle', vehicle)
+    .use('/pedido', pedido)
 }
